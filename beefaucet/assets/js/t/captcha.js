e8c71406607ab8f12ad418ec5c46a52f05ef7e47
hari54321 <69819823+hari54321@users.noreply.github.com>
2021-07-10 02:04:47 +0530

    Crypto Faucet

--- beefaucet/assets/js/t/captcha.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    var curr = $('#selectCaptcha').val();
-    $(`#${curr}`).css('display', 'block');
-});
-$('#selectCaptcha').change(function () {
-    $('.captcha').css('display', 'none');
-    var curr = $(this).val();
-    $(`#${curr}`).css('display', 'block');
-});
